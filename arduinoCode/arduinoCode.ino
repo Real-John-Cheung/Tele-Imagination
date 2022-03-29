3bdcc6b5903bc3185856d989964106b10cee8ef9
JohnC <real.john.chueng@gmail.com>
2022-03-29 18:15:44 +0800

    cam server

--- arduinoCode/arduinoCode.ino
+++ arduinoCode/arduinoCode.ino
@@ -1,11 +1,181 @@
+// based on https://stackoverflow.com/questions/60029614/esp32-cam-stream-in-opencv-python
 #include <Servo.h>
+#include "CameraMovement.h"
+#include <esp32cam.h>
+#include <WebServer.h>
+#include <WiFi.h>
 
-#include <CameraMovement.h>
-CameraMovement camove();
-void setup(){
-  camove.getNext();
+const int bigPin = 5;   // TBD
+const int smallPin = 6; // TBD
+const int servoMoveGap = 100; //TBD
+unsigned long timer = 0;
+
+const char *WIFI_SSID = "";
+const char *WIFI_PASS = "";
+
+WebServer server(80);
+
+static auto loRes = esp32cam::Resolution::find(320, 640);
+static auto hiRes = esp32cam::Resolution::find(800, 600);
+
+int *servoArr[2] = {0, 0};
+CameraMovement camMo();
+Servo big();
+Servo small();
+
+void setup()
+{
+  // servo
+  big.attach(bigPin);
+  small.attach(smallPin);
+
+  // esp
+  // for db
+  Serial.begin(115200);
+  Serial.println();
+  {
+    using namespace esp32cam;
+    Config cfg;
+    cfg.setPins(pins::AiThinker);
+    cfg.setResolution(hiRes);
+    cfg.setBufferCount(2);
+    cfg.setJpeg(80);
+
+    bool ok = Camera.begin(cfg);
+    Serial.println(ok ? "OK" : "CamFail");
+  }
+
+  WiFi.persistent(false);
+  WiFi.mode(WIFI_STA);
+  WIFI.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+  }
+
+  // db
+  Serial.print("http://");
+  Serial.println(WiFi.localIP());
+  Serial.println("  /cam.bmp");
+  Serial.println("  /cam-lo.jpg");
+  Serial.println("  /cam-hi.jpg");
+  Serial.println("  /cam.mjpeg");
+
+  server.on("/cam.bmp", handleBmp);
+  server.on("/cam-lo.jpg", handleJpgLo);
+  server.on("/cam-hi.jpg", handleJpgHi);
+  server.on("/cam.jpg", handleJpg);
+  server.on("/cam.mjpeg", handleMjpeg);
+
+  server.begin();
 }
 
-void loop(){
+void loop()
+{
+  server.handleClient();
+  if (millis() - timer > servoMoveGap) {
+    cameraMove();
+    timer = millis();
+  }
+}
 
+void cameraMove()
+{
+  camMo.getNext(servoArr);
+  big.write(servoArr[0]);
+  small.write(servoArr[1]);
+}
+
+void handleBmp()
+{
+  if (!esp32cam::Camera.changeResolution(loRes))
+  {
+    Serial.println("SET-LO-RES FAIL");
+  }
+
+  auto frame = esp32cam::capture();
+  if (frame == nullptr)
+  {
+    Serial.println("CAPTURE FAIL");
+    server.send(503, "", "");
+    return;
+  }
+  Serial.printf("CAPTURE OK %dx%d %db\n", frame->getWidth(), frame->getHeight(),
+                static_cast<int>(frame->size()));
+
+  if (!frame->toBmp())
+  {
+    Serial.println("CONVERT FAIL");
+    server.send(503, "", "");
+    return;
+  }
+  Serial.printf("CONVERT OK %dx%d %db\n", frame->getWidth(), frame->getHeight(),
+                static_cast<int>(frame->size()));
+
+  server.setContentLength(frame->size());
+  server.send(200, "image/bmp");
+  WiFiClient client = server.client();
+  frame->writeTo(client);
+}
+
+void serveJpg()
+{
+  auto frame = esp32cam::capture();
+  if (frame == nullptr)
+  {
+    Serial.println("CAPTURE FAIL");
+    server.send(503, "", "");
+    return;
+  }
+  Serial.printf("CAPTURE OK %dx%d %db\n", frame->getWidth(), frame->getHeight(),
+                static_cast<int>(frame->size()));
+
+  server.setContentLength(frame->size());
+  server.send(200, "image/jpeg");
+  WiFiClient client = server.client();
+  frame->writeTo(client);
+}
+
+void handleJpgLo()
+{
+  if (!esp32cam::Camera.changeResolution(loRes))
+  {
+    Serial.println("SET-LO-RES FAIL");
+  }
+  serveJpg();
+}
+
+void handleJpgHi()
+{
+  if (!esp32cam::Camera.changeResolution(hiRes))
+  {
+    Serial.println("SET-HI-RES FAIL");
+  }
+  serveJpg();
+}
+
+void handleJpg()
+{
+  server.sendHeader("Location", "/cam-hi.jpg");
+  server.send(302, "", "");
+}
+
+void handleMjpeg()
+{
+  if (!esp32cam::Camera.changeResolution(hiRes))
+  {
+    Serial.println("SET-HI-RES FAIL");
+  }
+
+  Serial.println("STREAM BEGIN");
+  WiFiClient client = server.client();
+  auto startTime = millis();
+  int res = esp32cam::Camera.streamMjpeg(client);
+  if (res <= 0)
+  {
+    Serial.printf("STREAM ERROR %d\n", res);
+    return;
+  }
+  auto duration = millis() - startTime;
+  Serial.printf("STREAM END %dfrm %0.2ffps\n", res, 1000.0 * res / duration);
 }
