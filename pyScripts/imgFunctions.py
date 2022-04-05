1b9aec6ee80d38be3384ab5f2fc6d90f07b839ab
John Cheung <61215358+Real-John-Cheung@users.noreply.github.com>
2022-04-05 14:30:04 +0800

    cleanup

--- pyScripts/imgFunctions.py
+++ pyScripts/imgFunctions.py
@@ -27,13 +27,3 @@ def close(window):
         cv2.destroyWindow(window)
     else:
         cv2.destroyAllWindows()
-
-# while True:
-#     imgResp=urllib.request.urlopen(url)
-#     imgNp=np.array(bytearray(imgResp.read()),dtype=np.uint8)
-#     img=cv2.imdecode(imgNp,-1)
-
-#     # all the opencv processing is done here
-#     cv2.imshow('test',img)
-#     if ord('q')==cv2.waitKey(10):
-#         exit(0)
